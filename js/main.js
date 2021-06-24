4e2eb43046f5ec8bf408bd00f0204356e3e65eaf
Doniyor-coder <asqaraliyev19991022@gmail.com~>
2021-06-24 11:33:09 +0500

    section-menu WIP

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ document.querySelector('.header__burger').addEventListener('click', () => {
   document.querySelector('.sitenav').classList.toggle('active')
   document.querySelector('.top-header').classList.toggle('active')
 })
+
+var coll = document.getElementsByClassName('sitenav-link')
+var i
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+    var content = this.nextElementSibling
+    if (content.style.display === 'block') {
+      content.style.display = 'none'
+    } else {
+      content.style.display = 'block'
+    }
+  })
+}
